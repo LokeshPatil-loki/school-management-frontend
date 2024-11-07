e3722e27fee8de0490f46ce49ae9b61a3fad47d7
LokeshPatil-loki <lokesh1404@outlook.com>
2024-11-08 00:00:37 +0530

    added user cards

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,7 +1,24 @@
+import UserCard from "@/components/UserCard";
+import { UserRole } from "@/lib/types/UserRole";
 import React from "react";
 
 const AdminPage = () => {
-  return <div>AdminPage</div>;
+  return (
+    <div className=" p-4 flex flex-col md:flex-row">
+      {/* LEFT */}
+      <div className=" w-full lg:w-2/3">
+        {/* USER CARD */}
+        <div className="flex gap-4 justify-between flex-wrap">
+          <UserCard type="staff" />
+          <UserCard type="teacher" />
+          <UserCard type="parent" />
+          <UserCard type="student" />
+        </div>
+      </div>
+      {/* RIGHT */}
+      <div className=" w-full lg:w-1/3">right</div>
+    </div>
+  );
 };
 
 export default AdminPage;
