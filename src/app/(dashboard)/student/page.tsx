aa5df2276d188c17fbd92084a770c6e2c04750ac
LokeshPatil-loki <lokesh1404@outlook.com>
2024-11-10 22:51:21 +0530

    Added Student Page

--- src/app/(dashboard)/student/page.tsx
+++ src/app/(dashboard)/student/page.tsx
@@ -1,7 +1,26 @@
+import Announcements from "@/components/Announcements";
+import BigCalender from "@/components/BigCalender";
+import EventCalendar from "@/components/EventCalendar";
 import React from "react";
 
 const StudentPage = () => {
-  return <div>StudentPage</div>;
+  return (
+    <div className="p-4 flex flex-col xl:flex-row gap-4">
+      {/* LEFT */}
+      <div className="w-full xl:w-2/3">
+        <div className="h-full bg-white p-4 rounded-md">
+          <h1 className="text-xl font-semibold">Schedule (4A)</h1>
+          <BigCalender />
+        </div>
+      </div>
+
+      {/* RIGHT */}
+      <div className="w-full xl:w-1/3 flex flex-col gap-8">
+        <EventCalendar />
+        <Announcements />
+      </div>
+    </div>
+  );
 };
 
 export default StudentPage;
