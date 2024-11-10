3d3d36e999f7d895ed7f3e1dbd1edb654f82593b
LokeshPatil-loki <lokesh1404@outlook.com>
2024-11-11 01:40:12 +0530

    created teachers list page

---
+++ src/lib/types/Teacher.ts
@@ -0,0 +1,12 @@
+export type Teacher = {
+  id: number;
+  teacherId: string;
+  name: string;
+  email?: string;
+  photo: string;
+  font: string;
+  subjects: string[];
+  classes: string[];
+  address: string;
+  phone: string;
+};
