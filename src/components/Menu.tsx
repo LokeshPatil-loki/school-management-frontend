ab5027284b32e9691acb3535f64f9261d84603a0
LokeshPatil-loki <lokesh1404@outlook.com>
2024-11-10 11:39:26 +0530

    fixed navbar

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,3 +1,4 @@
+import { role } from "@/lib/data";
 import { UserRole } from "@/lib/types/UserRole";
 import Image from "next/image";
 import Link from "next/link";
@@ -182,16 +183,24 @@ const Menu = () => {
           <span className="hidden lg:block text-gray-400 font-light my-4">
             {i.title}
           </span>
-          {i.items.map((item) => (
-            <Link
-              className="flex items-center justify-center lg:justify-start gap-4  text-gray-500 py-1"
-              href={item.href}
-              key={item.label}
-            >
-              <Image src={item.icon} alt={item.label} width={20} height={20} />
-              <span className="hidden lg:block">{item.label}</span>
-            </Link>
-          ))}
+          {i.items.map(
+            (item) =>
+              item.visible.includes(role) && (
+                <Link
+                  className="flex items-center justify-center lg:justify-start gap-4  text-gray-500 py-1 md:p-2 rounded-md hover:bg-skyLight"
+                  href={item.href}
+                  key={item.label}
+                >
+                  <Image
+                    src={item.icon}
+                    alt={item.label}
+                    width={20}
+                    height={20}
+                  />
+                  <span className="hidden lg:block">{item.label}</span>
+                </Link>
+              )
+          )}
         </div>
       ))}
     </div>
