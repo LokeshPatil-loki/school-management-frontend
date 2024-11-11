df2bec333957af99a79673daadb6b322e2b369d3
LokeshPatil-loki <lokesh1404@outlook.com>
2024-11-11 11:21:20 +0530

    created students list page

---
+++ src/lib/types/Student.ts
@@ -0,0 +1,11 @@
+export type Student = {
+  id: number;
+  studentId: string;
+  email?: string;
+  name: string;
+  photo: string;
+  phone?: string;
+  grade: number;
+  class: string;
+  address: string;
+};
