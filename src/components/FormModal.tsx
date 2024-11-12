dcca592732add883a8f8706586d7d20822a84b1a
LokeshPatil-loki <lokesh1404@outlook.com>
2024-11-12 10:37:27 +0530

    Created Student Form

--- src/components/FormModal.tsx
+++ src/components/FormModal.tsx
@@ -1,6 +1,15 @@
 "use client";
 import Image from "next/image";
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
+import TeacherForm from "./forms/TeacherForm";
+import StudentForm from "./forms/StudentForm";
+
+const forms: {
+  [key: string]: (type: "create" | "update", data?: any) => JSX.Element;
+} = {
+  teacher: (type, data) => <TeacherForm type={type} data={data} />,
+  student: (type, data) => <StudentForm type={type} data={data} />,
+};
 
 interface FormModalProps {
   table:
@@ -29,12 +38,68 @@ const FormModal = ({ table, type, data, id }: FormModalProps) => {
       : type === "update"
       ? "bg-sky"
       : "bg-purple";
+  const [open, setOpen] = useState(false);
+  const modalRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        modalRef.current &&
+        !modalRef.current.contains(event.target as Node)
+      ) {
+        setOpen(false);
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  const Form = () => {
+    if (type === "delete" && id) {
+      return (
+        <form className="flex flex-col gap-4 p-4" action={""}>
+          <span className="text-center">
+            All data will be lost. Are you sure you want to delete this {table}
+          </span>
+          <button className="bg-red-700 text-white py-2 px-4 rounded-md border-none w-max self-center">
+            Delete
+          </button>
+        </form>
+      );
+    } else if (type !== "delete") {
+      return forms[table](type, data);
+    } else {
+      return "Form not found";
+    }
+  };
+
   return (
-    <button
-      className={`${size} flex items-center justify-center rounded-full ${bgColor}`}
-    >
-      <Image src={`/${type}.png`} alt="" width={16} height={16} />
-    </button>
+    <>
+      <button
+        className={`${size} flex items-center justify-center rounded-full ${bgColor}`}
+        onClick={() => setOpen(true)}
+      >
+        <Image src={`/${type}.png`} alt="" width={16} height={16} />
+      </button>
+      {open && (
+        <div className="w-screen h-screen absolute left-0 top-0 bg-black bg-opacity-60 z-50 flex items-center justify-center">
+          <div
+            ref={modalRef}
+            className="bg-white p-4 rounded-md relative w-[90%] md:w-[70%] lg:w-[60%] xl:w-[50%] 2xl:w-[40%]"
+          >
+            <div
+              onClick={() => setOpen(false)}
+              className="absolute t-4 right-4 cursor-pointer"
+            >
+              <Image src={"/close.png"} alt="" width={14} height={14} />
+            </div>
+            <Form />
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
