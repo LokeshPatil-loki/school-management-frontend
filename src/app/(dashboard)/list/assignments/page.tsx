99b4cc66c9bbaedc42bcc9439b2d62f50e57549d
LokeshPatil-loki <lokesh1404@outlook.com>
2024-11-27 21:27:32 +0530

    fetch assignments

--- src/app/(dashboard)/list/assignments/page.tsx
+++ src/app/(dashboard)/list/assignments/page.tsx
@@ -1,27 +1,14 @@
+"use client";
 import FormModal from "@/components/FormModal";
 import Pagination from "@/components/Pagination";
 import Table from "@/components/Table";
 import TableSearch from "@/components/TableSearch";
-import {
-  assignmentsData,
-  classesData,
-  examsData,
-  lessonsData,
-  parentsData,
-  role,
-  studentsData,
-} from "@/lib/data";
+import { assignmentsData, role } from "@/lib/data";
+import { useGetAssginmentsQuery } from "@/lib/redux/api/assignmentsApiSlices";
 import { Assignment } from "@/lib/types/models/Assignment";
-import { Classes } from "@/lib/types/models/Classes";
-import { Exam } from "@/lib/types/models/Exam";
-import { Lesson } from "@/lib/types/models/Lesson";
-import { Parent } from "@/lib/types/models/Parent";
-import { Student } from "@/lib/types/models/Student";
-import { Teacher } from "@/lib/types/models/Teacher";
 import { UserRole } from "@/lib/types/models/Enums";
 import Image from "next/image";
-import Link from "next/link";
-import { it } from "node:test";
+import { useSearchParams } from "next/navigation";
 import React from "react";
 
 const columns = [
@@ -49,35 +36,47 @@ const columns = [
   },
 ];
 
-const AssignmentsListPage = () => {
-  const renderRow = (item: Assignment) => {
-    return (
-      <tr
-        key={item.id}
-        className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-purpleLight"
-      >
-        <td className="flex flex-row items-center gap-4 p-4">
-          <div className="flex flex-col">
-            <h3 className="font-semibold">{item.subject}</h3>
-          </div>
-        </td>
-        <td className="table-cell text-sm">{item.class}</td>
-        <td className="hidden md:table-cell text-sm">{item.teacher}</td>
-        <td className="hidden md:table-cell text-sm">{item.dueDate}</td>
+const renderRow = (item: Assignment) => {
+  return (
+    <tr
+      key={item.id}
+      className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-purpleLight"
+    >
+      <td className="flex flex-row items-center gap-4 p-4">
+        <div className="flex flex-col">
+          <h3 className="font-semibold">{item.lesson.subject.name}</h3>
+        </div>
+      </td>
+      <td className="table-cell text-sm">{item.lesson.class.name}</td>
+      <td className="hidden md:table-cell text-sm">
+        {item.lesson.teacher.name}
+      </td>
+      <td className="hidden md:table-cell text-sm">
+        {new Intl.DateTimeFormat().format(new Date(item.dueDate.toString()))}
+      </td>
 
-        <td className="">
-          <div className="flex items-center gap-2">
-            {role === UserRole.admin && (
-              <>
-                <FormModal type="update" table="assignment" data={item} />
-                <FormModal type="delete" table="assignment" id={item.id} />
-              </>
-            )}
-          </div>
-        </td>
-      </tr>
-    );
-  };
+      <td className="">
+        <div className="flex items-center gap-2">
+          {role === UserRole.admin && (
+            <>
+              <FormModal type="update" table="assignment" data={item} />
+              <FormModal type="delete" table="assignment" id={item.id} />
+            </>
+          )}
+        </div>
+      </td>
+    </tr>
+  );
+};
+const AssignmentsListPage = () => {
+  const searchParams = useSearchParams();
+  const { data, isLoading, isFetching } = useGetAssginmentsQuery({
+    classId: searchParams.get("classId"),
+    limit: searchParams.get("limit"),
+    page: searchParams.get("page"),
+    search: searchParams.get("search"),
+    teacherId: searchParams.get("teacherId"),
+  });
   return (
     <div className="w-[97%] h-[98%] m-4 mx-auto bg-white p-4">
       {/* TOP */}
@@ -101,9 +100,15 @@ const AssignmentsListPage = () => {
         </div>
       </div>
       {/* LIST */}
-      <Table columns={columns} renderRow={renderRow} data={assignmentsData} />
-      {/* PAGINATION */}
-      <Pagination />
+      {!isLoading && (
+        <>
+          <Table columns={columns} renderRow={renderRow} data={data.data} />
+          <Pagination
+            currentPage={data?.meta?.currentPage}
+            totalPages={data?.meta?.totalPages}
+          />
+        </>
+      )}
     </div>
   );
 };
