65d9a77995ae53b6371dad5bc924879077b6ba40
LokeshPatil-loki <lokesh1404@outlook.com>
2024-11-27 22:36:37 +0530

    fetch results

--- src/app/(dashboard)/list/results/page.tsx
+++ src/app/(dashboard)/list/results/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import FormModal from "@/components/FormModal";
 import Pagination from "@/components/Pagination";
 import Table from "@/components/Table";
@@ -8,11 +9,14 @@ import { UserRole } from "@/lib/types/models/Enums";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { useSearchParams } from "next/navigation";
+import { useGetResultsQuery } from "@/lib/redux/api/resultsApiSlices";
+import { title } from "process";
 
 const columns = [
   {
-    header: "Subject Name",
-    accessor: "name",
+    header: "title",
+    accessor: "title",
   },
   {
     header: "Student",
@@ -44,39 +48,87 @@ const columns = [
   },
 ];
 
-const ResultsListPage = () => {
-  const renderRow = (item: Result) => {
-    return (
-      <tr
-        key={item.id}
-        className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-purpleLight"
-      >
-        <td className="flex flex-row items-center gap-4 p-4">
-          <div className="flex flex-col">
-            <h3 className="font-semibold">{item.subject}</h3>
-          </div>
-        </td>
-        <td className="table-cell text-sm">{item.student}</td>
-        <td className="hidden md:table-cell text-sm">{item.score}</td>
-        <td className="hidden md:table-cell text-sm">{item.teacher}</td>
-        <td className="hidden md:table-cell text-sm">{item.class}</td>
-        <td className="hidden md:table-cell text-sm">{item.date}</td>
+type ResultList = {
+  id: string;
+  title: string;
+  studentName: string;
+  studentSurname: string;
+  teacherName: string;
+  teacherSurname: string;
+  score: string;
+  className: string;
+  startTime: Date;
+};
 
-        <td className="">
-          <div className="flex items-center gap-2">
-            <Link href={`/list/teachers/${item.id}`}>
-              <button className="w-7 h-7 flex items-center justify-center rounded-full bg-sky">
-                <Image src={"/edit.png"} alt="" width={16} height={16} />
-              </button>
-            </Link>
-            {role === UserRole.admin && (
-              <FormModal type="delete" table="result" id={item.id} />
-            )}
-          </div>
-        </td>
-      </tr>
-    );
-  };
+const renderRow = (item: ResultList) => {
+  return (
+    <tr
+      key={item.id}
+      className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-purpleLight"
+    >
+      <td className="flex flex-row items-center gap-4 p-4">
+        <div className="flex flex-col">
+          <h3 className="font-semibold">{item.title}</h3>
+        </div>
+      </td>
+      <td className="table-cell text-sm">
+        {item.studentName + " " + item.studentSurname}
+      </td>
+      <td className="hidden md:table-cell text-sm">{item.score}</td>
+      <td className="hidden md:table-cell text-sm">
+        {item.teacherName + " " + item.teacherSurname}
+      </td>
+      <td className="hidden md:table-cell text-sm">{item.className}</td>
+      <td className="hidden md:table-cell text-sm">
+        {new Intl.DateTimeFormat().format(new Date(item.startTime))}
+      </td>
+
+      <td className="">
+        <div className="flex items-center gap-2">
+          <Link href={`/list/teachers/${item.id}`}>
+            <button className="w-7 h-7 flex items-center justify-center rounded-full bg-sky">
+              <Image src={"/edit.png"} alt="" width={16} height={16} />
+            </button>
+          </Link>
+          {role === UserRole.admin && (
+            <FormModal type="delete" table="result" id={item.id} />
+          )}
+        </div>
+      </td>
+    </tr>
+  );
+};
+const ResultsListPage = () => {
+  const searchParams = useSearchParams();
+  const {
+    data: dataRes,
+    isLoading,
+    isSuccess,
+    isError,
+  } = useGetResultsQuery({
+    limit: searchParams.get("limit"),
+    page: searchParams.get("page"),
+    search: searchParams.get("search"),
+    studentId: searchParams.get("studentId"),
+  });
+  let data = dataRes?.data?.map((item: Result) => {
+    const assessment = item.exam || item.assignment;
+    if (!assessment) return null;
+
+    const isExam = "startTime" in assessment;
+
+    return {
+      id: item.id,
+      title: assessment.title,
+      studentName: item.student.name,
+      studentSurname: item.student.surname,
+      teacherName: assessment.lesson.teacher.name,
+      teacherSurname: assessment.lesson.teacher.surname,
+      score: item.score,
+      className: assessment.lesson.class.name,
+      startTime: isExam ? assessment.startTime : assessment.startDate,
+    };
+  });
   return (
     <div className="w-[97%] h-[98%] m-4 mx-auto bg-white p-4">
       {/* TOP */}
@@ -98,9 +150,15 @@ const ResultsListPage = () => {
         </div>
       </div>
       {/* LIST */}
-      <Table columns={columns} renderRow={renderRow} data={resultsData} />
-      {/* PAGINATION */}
-      <Pagination />
+      {!isLoading && isSuccess && (
+        <>
+          <Table columns={columns} renderRow={renderRow} data={data} />
+          <Pagination
+            currentPage={data?.meta?.currentPage}
+            totalPages={data?.meta?.totalPages}
+          />
+        </>
+      )}
     </div>
   );
 };
