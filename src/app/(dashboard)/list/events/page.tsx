6a0466a5ddf874d275c774d3045581b69771193c
LokeshPatil-loki <lokesh1404@outlook.com>
2024-11-28 10:36:51 +0530

    fetch events

--- src/app/(dashboard)/list/events/page.tsx
+++ src/app/(dashboard)/list/events/page.tsx
@@ -1,12 +1,16 @@
+"use client";
 import FormModal from "@/components/FormModal";
 import Pagination from "@/components/Pagination";
 import Table from "@/components/Table";
 import TableSearch from "@/components/TableSearch";
 import { eventsData, role } from "@/lib/data";
-import { Event } from "@/lib/types/models/Events";
+import { useGetEventsQuery } from "@/lib/redux/api/eventsApiSlice";
+import { Event } from "@/lib/types/models";
 import { UserRole } from "@/lib/types/models/Enums";
+import { formatDate, formatTime } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
+import { useSearchParams } from "next/navigation";
 import React from "react";
 
 const columns = [
@@ -38,36 +42,49 @@ const columns = [
     accessor: "action",
   },
 ];
-const EventsListPage = () => {
-  const renderRow = (item: Event) => {
-    return (
-      <tr
-        key={item.id}
-        className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-purpleLight"
-      >
-        <td className="flex flex-row items-center gap-4 p-4">
-          <div className="flex flex-col">
-            <h3 className="font-semibold">{item.title}</h3>
-          </div>
-        </td>
-        <td className="table-cell text-sm">{item.class}</td>
-        <td className="hidden md:table-cell text-sm">{item.date}</td>
-        <td className="hidden md:table-cell text-sm">{item.startTime}</td>
-        <td className="hidden md:table-cell text-sm">{item.endTime}</td>
+const renderRow = (item: Event) => {
+  return (
+    <tr
+      key={item.id}
+      className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-purpleLight"
+    >
+      <td className="flex flex-row items-center gap-4 p-4">
+        <div className="flex flex-col">
+          <h3 className="font-semibold">{item.title}</h3>
+        </div>
+      </td>
+      <td className="table-cell text-sm">{item.class?.name}</td>
+      <td className="hidden md:table-cell text-sm">
+        {formatDate(item.startTime)}
+      </td>
+      <td className="hidden md:table-cell text-sm">
+        {formatTime(item.startTime)}
+      </td>
+      <td className="hidden md:table-cell text-sm">
+        {formatTime(item.endTime)}
+      </td>
 
-        <td className="">
-          <div className="flex items-center gap-2">
-            {role === UserRole.admin && (
-              <>
-                <FormModal type="update" table="event" data={item} />
-                <FormModal type="delete" table="event" id={item.id} />
-              </>
-            )}
-          </div>
-        </td>
-      </tr>
-    );
-  };
+      <td className="">
+        <div className="flex items-center gap-2">
+          {role === UserRole.admin && (
+            <>
+              <FormModal type="update" table="event" data={item} />
+              <FormModal type="delete" table="event" id={item.id} />
+            </>
+          )}
+        </div>
+      </td>
+    </tr>
+  );
+};
+const EventsListPage = () => {
+  const searchParams = useSearchParams();
+  const { isLoading, data } = useGetEventsQuery({
+    page: searchParams.get("page") || "1",
+    limit: searchParams.get("limit") || undefined,
+    search: searchParams.get("search"),
+    classId: searchParams.get("classId"),
+  });
   return (
     <div className="w-[97%] h-[98%] m-4 mx-auto bg-white p-4">
       {/* TOP */}
@@ -89,9 +106,15 @@ const EventsListPage = () => {
         </div>
       </div>
       {/* LIST */}
-      <Table columns={columns} renderRow={renderRow} data={eventsData} />
-      {/* PAGINATION */}
-      <Pagination />
+      {!isLoading && (
+        <>
+          <Table columns={columns} renderRow={renderRow} data={data.data} />
+          <Pagination
+            currentPage={data?.meta?.currentPage}
+            totalPages={data?.meta?.totalPages}
+          />
+        </>
+      )}
     </div>
   );
 };
