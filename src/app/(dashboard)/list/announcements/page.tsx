11ddbd424e8596a2f21f86b6a5805247957b30ca
LokeshPatil-loki <lokesh1404@outlook.com>
2024-11-28 10:47:49 +0530

    fetched announcments

--- src/app/(dashboard)/list/announcements/page.tsx
+++ src/app/(dashboard)/list/announcements/page.tsx
@@ -1,14 +1,18 @@
+"use client";
 import FormModal from "@/components/FormModal";
 import Pagination from "@/components/Pagination";
 import Table from "@/components/Table";
 import TableSearch from "@/components/TableSearch";
 import { announcementsData, eventsData, role } from "@/lib/data";
 import { Announcement } from "@/lib/types/models/Announcement";
-import { Event } from "@/lib/types/models/Events";
+import { Event } from "@/lib/types/models";
 import { UserRole } from "@/lib/types/models/Enums";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { formatDate } from "@/lib/utils";
+import { useSearchParams } from "next/navigation";
+import { useGetAnnouncmentsQuery } from "@/lib/redux/api/announcmentsApiSlice";
 
 const columns = [
   {
@@ -30,34 +34,41 @@ const columns = [
   },
 ];
 
-const AnnouncementsListPage = () => {
-  const renderRow = (item: Announcement) => {
-    return (
-      <tr
-        key={item.id}
-        className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-purpleLight"
-      >
-        <td className="flex flex-row items-center gap-4 p-4">
-          <div className="flex flex-col">
-            <h3 className="font-semibold">{item.title}</h3>
-          </div>
-        </td>
-        <td className="table-cell text-sm">{item.class}</td>
-        <td className="hidden md:table-cell text-sm">{item.date}</td>
+const renderRow = (item: Announcement) => {
+  return (
+    <tr
+      key={item.id}
+      className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-purpleLight"
+    >
+      <td className="flex flex-row items-center gap-4 p-4">
+        <div className="flex flex-col">
+          <h3 className="font-semibold">{item.title}</h3>
+        </div>
+      </td>
+      <td className="table-cell text-sm">{item.class?.name}</td>
+      <td className="hidden md:table-cell text-sm">{formatDate(item.date)}</td>
 
-        <td className="">
-          <div className="flex items-center gap-2">
-            {role === UserRole.admin && (
-              <>
-                <FormModal type="update" table="teacher" data={item} />
-                <FormModal type="delete" table="teacher" id={item.id} />
-              </>
-            )}
-          </div>
-        </td>
-      </tr>
-    );
-  };
+      <td className="">
+        <div className="flex items-center gap-2">
+          {role === UserRole.admin && (
+            <>
+              <FormModal type="update" table="teacher" data={item} />
+              <FormModal type="delete" table="teacher" id={item.id} />
+            </>
+          )}
+        </div>
+      </td>
+    </tr>
+  );
+};
+const AnnouncementsListPage = () => {
+  const searchParams = useSearchParams();
+  const { isLoading, data } = useGetAnnouncmentsQuery({
+    page: searchParams.get("page") || "1",
+    limit: searchParams.get("limit") || undefined,
+    search: searchParams.get("search"),
+    classId: searchParams.get("classId"),
+  });
   return (
     <div className="w-[97%] h-[98%] m-4 mx-auto bg-white p-4">
       {/* TOP */}
@@ -81,9 +92,15 @@ const AnnouncementsListPage = () => {
         </div>
       </div>
       {/* LIST */}
-      <Table columns={columns} renderRow={renderRow} data={announcementsData} />
-      {/* PAGINATION */}
-      <Pagination />
+      {!isLoading && (
+        <>
+          <Table columns={columns} renderRow={renderRow} data={data.data} />
+          <Pagination
+            currentPage={data?.meta?.currentPage}
+            totalPages={data?.meta?.totalPages}
+          />
+        </>
+      )}
     </div>
   );
 };
