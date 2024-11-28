f3b26650db2a720904ae83706524e29c53e3a34f
LokeshPatil-loki <lokesh1404@outlook.com>
2024-11-28 18:43:18 +0530

    added clerk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import StoreProvider from "./StoreProvider";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,10 +17,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <StoreProvider>
-        <body className={inter.className}>{children}</body>
-      </StoreProvider>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <StoreProvider>
+          <body className={inter.className}>{children}</body>
+        </StoreProvider>
+      </html>
+    </ClerkProvider>
   );
 }
